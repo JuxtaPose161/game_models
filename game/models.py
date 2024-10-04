0d47e52248907b2402649fda500c77727be0535f
Temik <paa161_j@mail.ru>
2024-10-04 12:56:57 +0300

    First commit)

---
+++ game/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+POINTS_FOR_JOIN = 100
+
+class Boost(models.Model):
+    title = models.CharField(max_length=30, db_index=True, unique=True)
+    factor = models.FloatField()
+def get_default_boost():
+    # Выдача базового множителя опыта
+    boost, created = Boost.objects.get_or_create(title='Default', factor=1.0)
+    return boost
+class Player(models.Model):
+
+
+    username = models.CharField(max_length=30, db_index=True, unique=True)
+    date_created = models.DateTimeField(default=timezone.localtime)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_joined_today = models.BooleanField(default=False)
+
+    points = models.PositiveIntegerField(default=0)
+    level = models.PositiveSmallIntegerField(default=1)
+    boost = models.ForeignKey('Boost', on_delete=models.SET_DEFAULT,
+                              default=get_default_boost, related_name='boost')
+
+    def give_points_for_joining(self):
+        # Начисление очков за регулярные посещения
+        if self.is_joined_today is False:
+            without_active = timezone.now() - self.date_joined
+            if without_active.days < 2:
+                self.points += POINTS_FOR_JOIN
+            self.is_joined_today = True
+
+    def update_level(self):
+        # В этом примере за каждые 1000 очков даётся уровень
+        self.level = self.points//1000 + 1
+
+    def update_boost_for_level(self):
+        # Обновление бустера с увеличением уровня
+        if self.level >= 100:
+            boost, created = Boost.objects.get_or_create(title='Legend', factor=2.0)
+        elif self.level >= 25:
+            boost, created = Boost.objects.get_or_create(title='Ultra', factor=1.5)
+        elif self.level >= 10:
+            boost, created = Boost.objects.get_or_create(title='Super', factor=1.25)
+        else:
+            boost = get_default_boost()
+        self.boost = boost
---
+++ game2/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+import csv
+class Player(models.Model):
+    player_id = models.CharField(max_length=100)
+    def take_a_prize(self, prize):
+        print( f"Player with id {self.player_id} take a {prize.title}")
+
+    def csv_dump(self):
+        headers = ["ID", "level_title", "is_completed", "prize"]
+        with open(f"{self.player_id}_stats.csv", mode='w', encoding='utf-8') as file:
+            file_writer = csv.DictWriter(file, delimiter=",", fieldnames=headers)
+            file_writer.writeheader()
+            for player_level in self.player.all().select_related('level'):
+                file_writer.writerow({"ID": self.player_id,
+                                      "level_title": player_level.level.title,
+                                     "is_completed": player_level.is_completed,
+                                      "prize": player_level.level.prize_level.first().prize.title})
+class Level(models.Model):
+    title = models.CharField(max_length=100)
+    order = models.IntegerField(default=0)
+
+class Prize(models.Model):
+    title = models.CharField(max_length=30)
+
+class PlayerLevel(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player')
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name='player_level')
+    completed = models.DateField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    score = models.PositiveIntegerField(default=0)
+
+    def complete_level(self):
+        self.completed = timezone.now()
+        self.is_completed = True
+        level_prize = self.level.prize_level.first()
+        print(level_prize)
+        self.player.take_a_prize(level_prize.prize)
+        level_prize.received = timezone.now()
+        self.save()
+
+class LevelPrize(models.Model):
+    level = models.ForeignKey(Level, on_delete=models.CASCADE, related_name='prize_level')
+    prize = models.ForeignKey(Prize, on_delete=models.CASCADE, related_name='prize')
+    received = models.DateField(blank=True, null=True)
+
+
+
+
